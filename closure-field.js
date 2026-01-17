f16cd1d1c3bd1f42b7223e0020385b599b0ae64c
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-01-18 01:43:30 +0200

    Create closure-field.js

---
+++ closure-field.js
@@ -0,0 +1,9 @@
+(function () {
+  window.HCM_CLOSURE = {
+    seal() {
+      // intentionally empty
+      // the absence of continuation is the closure
+      console.debug("HCM: event sealed into silence");
+    }
+  };
+})();
